3039a6db46ed7518085c44c364382afa3580467b
shivaB55 <shivabaalireddipalli@gmail.com>
2021-04-26 01:15:24 +0000

    third commit

---
+++ pythonscr.py
@@ -0,0 +1,7 @@
+#!bash/sh/python3
+import random
+name =input('enter the name')
+lis=['shiva','sai','manju','balaji','saikiran']
+print('hello ',name)
+print('you have entered name is matchs with',random.choice(lis))
+
