40cdc3261662abe6b9db6622e7daaa83bc6f7901
Cloud User <ec2-user@ip-172-31-36-234.ap-south-1.compute.internal>
2021-04-26 01:49:29 +0000

    Revert "third commit"
    This reverts commit 039c153481018b9001a37a192b33b033c539a7d1.

--- demo7.sh
+++
@@ -1,7 +0,0 @@
-s="hi shiva welcome to devops course"
-we will help u to get one good job
-java is the scrptingn language
-pyton is dynamicaly typed laungauge
-linux is the secure programme language
-echo ${#s}
-
